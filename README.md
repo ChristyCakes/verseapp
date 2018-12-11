6abbce33a3578fa925308ddfb5580223403c47de
Christy Welsh <christyannwelsh@gmail.com>
2018-12-11 12:23:49 -0800

    add modals

---
+++ README.md
@@ -0,0 +1,8 @@
+# Verse App
+This is a basic web app that utilizes the following tools:
+
+* React Router
+* Express.js
+* Passport Authentication
+* MongoDB
+* Design framework with Material UI
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -6,24 +6,33 @@ import Bubble from './bubble'
 import Button from '@material-ui/core/Button';
 // import { AuthButton } from './auth';
 import * as baseService from '../services/base';
+// import Modal from '@material-ui/core/Modal';
+// import PropTypes from 'prop-types';
+import SimpleModal from './simplemodal'
 
 class Home extends Component {
     constructor(props) {
-        super(props);    
-        this.state = { verses: "" }
-        this.postVerse = this.postVerse.bind(this)
-        this.inputHandler = this.inputHandler.bind(this)
+        super(props);
+        this.state = {
+            verses: "",
+            // open: false
+        }
+        this.postVerse = this.postVerse.bind(this);
+        this.inputHandler = this.inputHandler.bind(this);
+        // this.handleOpen = this.handleOpen.bind(this);
+        // this.handleClose = this.handleClose.bind(this);
     }
 
-    inputHandler(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+
+    // handleOpen = () => { this.setState({ open: true }) };
+    // handleClose = () => { this.setState({ open: false }) };
+    inputHandler(event) { this.setState({ [event.target.name]: event.target.value }) };
 
     postVerse(event) {
         event.preventDefault();
-        baseService.post('/api/verse', {verse: this.state.verse})
+        baseService.post('/api/verse', { verse: this.state.verse })
             // .then(response => { return this.props.history.push(`/api/verse/`) })
-            
+
             .catch(err => {
                 alert("Error: Your verse was not added");
                 console.log(err)
@@ -52,14 +61,32 @@ class Home extends Component {
                         defaultValue={this.state.verse}
                     />
                     <button
-                            onClick={this.postVerse}
-                            id="submit">
-                            Add
+                        onClick={this.postVerse}
+                        id="submit">
+                        Add
                     </button>
                 </form>
+                <SimpleModal color="secondary" label="angry"/>
+                <SimpleModal color="primary" label="happy"/>
+                {/* <Button onClick={this.handleOpen}>Open Modal</Button> */}
+                {/* <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                >Modal
+                </Modal>
+                <SimpleModalWrapped /> */}
             </div>
         )
     }
 }
+
+// SimpleModal.propTypes = {
+//     classes: PropTypes.object.isRequired,
+//   };
+  
+//   // We need an intermediary variable for handling the recursive nesting.
+//   const SimpleModalWrapped = withStyles(styles)(SimpleModal);
 
 export default Home;
