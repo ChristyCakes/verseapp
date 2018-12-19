293835461a42cf9034b963f3bae62702370e8c01
Christy Welsh <christyannwelsh@gmail.com>
2018-12-19 14:14:10 -0800

    format verse text

--- client/src/components/simplemodal.jsx
+++ client/src/components/simplemodal.jsx
@@ -9,12 +9,16 @@ import * as baseService from '../services/base';
 import pink from '@material-ui/core/colors/pink';
 import lightblue from '@material-ui/core/colors/lightblue';
 
-//const url = "https://bibles.org/v2/chapters/eng-CEV:1Cor.2/verses.js?start=5&end=7";
-//const url = "https://bibles.org/v2/verses/eng-GNTD:Acts.8.34.js";
+// const url = "https://bibles.org/v2/chapters/eng-CEV:1Cor.2/verses.js?start=5&end=7";
+// const url = "https://bibles.org/v2/verses/eng-GNTD:Acts.8.34.js";
 // const url = "https://bibles.org/v2/passages.js?q[]=john+3:1-5&version=eng-CEV"
 const proxyurl = "https://serene-crag-81882.herokuapp.com/"
-// const url = `https://bibles.org/v2/verses/eng-CEV:${this.state.reference}.js`;
 const BIBLE_API_KEY = process.env.BIBLE_API_KEY
+
+var parser = new DOMParser();
+
+var versecontent = [];
+var versetext = "";
 
 function rand() {
     return Math.round(Math.random() * 20) - 10;
@@ -62,7 +66,7 @@ class SimpleModal extends React.Component {
     // onClick of emotion button, 3 steps occur:
     handleOpen = () => {
 
-        // call express api, returns a random reference from specified emotion document
+        // 1. call express api, returns a random reference from specified emotion document
         baseService.get(`/api/passages/${this.props.document}`)
             .then(data => {
                 this.setState({
@@ -73,9 +77,9 @@ class SimpleModal extends React.Component {
                 })
             })
 
-            // call Bible api with reference, returns verse content
+            // 2. call Bible api with reference, returns verse content
             .then(() => {
-                const url = `https://bibles.org/v2/chapters/eng-CEV:${this.state.abbr}/verses.js?start=${this.state.start}&end=${this.state.end}`
+                const url = `https://bibles.org/v2/chapters/eng-NASB:${this.state.abbr}/verses.js?start=${this.state.start}&end=${this.state.end}`
                 fetch(proxyurl + url, {
                     headers: new Headers({ 'Authorization': 'Basic ' + window.btoa(`${BIBLE_API_KEY}: x`) }),
                     redirect: "follow",
@@ -84,19 +88,19 @@ class SimpleModal extends React.Component {
                     .then(contents => {
                         let versearray = contents.response.verses
                         versearray.forEach(verse => {
-                            this.setState({ 
-                                content: [...this.state.content, verse.text] })
+                            var regex = /(<([^>]+)>)|[0-9]/ig;
+                            let versetext = verse.text.replace(regex, "");
+                            let alltext = this.state.content.concat(' ', versetext)
+                            this.setState({
+                                content: alltext,
+
+                                // 3. open modal displaying reference and verse content
+                                open: true
+                            })
                         })
-                        // console.log(this.state.content)  -- array of strings
-
-                        //     this.setState({ content: contents.response.verses[0].text })
-            
                     })
-                    .catch((e) => console.log(`Can’t access Bible API url. Error: ${e}`))
+                    .catch((e) => console.log(`Error: ${e}`))
             })
-
-            // open modal displaying reference and verse content
-            .then(() => this.setState({ open: true }))
             .catch(err => {
                 alert("Your Verse Failed to Load");
                 console.log(err)
@@ -117,7 +121,9 @@ class SimpleModal extends React.Component {
                 <Modal open={this.state.open} onClose={this.handleClose}>
                     <div style={getModalStyle()} className={classes.paper}>
                         <Typography variant="h6" id="modal-title">{this.state.reference}</Typography>
-                        <Typography variant="subtitle1" id="simple-modal-description"><div dangerouslySetInnerHTML={{ __html: this.state.content }} /></Typography>
+                        {/* <Typography variant="subtitle1" id="simple-modal-description"><div dangerouslySetInnerHTML={{ __html: this.state.content }} /></Typography> */}
+                        <Typography variant="subtitle1" id="simple-modal-description">{this.state.content}</Typography>
+
                     </div>
                 </Modal>
             </div>
@@ -133,3 +139,45 @@ SimpleModal.propTypes = {
 const SimpleModalWrapped = withStyles(styles)(SimpleModal);
 
 export default SimpleModalWrapped;
+
+
+
+
+
+
+
+
+
+// let joined = versecontent.join(" ")
+// this.setState({
+//     content: joined
+// })
+
+
+
+
+
+
+
+
+
+// REGEX METHOD
+// .then(res => res.json())                                                // convert api json response to js
+//     .then(contents => {
+//         let versearray = contents.response.verses                           // extract array of verses objects
+
+//         versearray.forEach(verse => {                                       // loop through array
+//             let versetext = verse.text                                      // extract text value for each verse object (includes other unwanted items)
+//             console.log(versetext)
+
+//             let pattern = /<\/sup\b[^>]*>(.*?)<\/p>/
+//             let words = pattern.exec(versetext)[1]
+//             this.setState({
+//                 content: [...this.state.content, words]
+//             })
+//             console.log(this.state.content)
+//         })
+//     })
+//     .catch((e) => console.log(`Can’t access Bible API url. Error: ${e}`))
+
+{/* <Typography variant="subtitle1" id="simple-modal-description">{this.state.content}</Typography> */ }
