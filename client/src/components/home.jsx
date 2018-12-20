725a61fbc0de283c7eb5b41e1dc615f12f99a3d4
Christy Welsh <christyannwelsh@gmail.com>
2018-12-19 20:04:56 -0800

    add color themes

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import SimpleModal from './simplemodal';
 import 'isomorphic-fetch';
+// import ColorTheme from './colortheme'
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const colortheme = createMuiTheme({
+    palette: {
+        primary: { main:'#f50057' },
+        secondary: { main:'#03a9f4' },
+        contrastText: '#fff',
+    }
+});
+
+const lightcolors = createMuiTheme({
+    palette: {
+        primary: { main:'#f44336' },
+        secondary: { main:'#00bcd4' },
+        contrastText: '#fff'
+    }
+})
 
 class Home extends Component {
     constructor(props) {
@@ -11,8 +30,10 @@ class Home extends Component {
         return (
             <div>
                 <h1 className='grey center'>How Do You Feel?</h1>
-                <SimpleModal label="angry" color="primary" document="anger" />
-                <SimpleModal label="content" color="secondary" document="content" />
+                <SimpleModal label="angry" theme={colortheme} color="primary" document="anger" />
+                <SimpleModal label="content" theme={colortheme} color="secondary" document="content" />
+                <SimpleModal label="stressed" theme={lightcolors} color="primary" document="stress"  />
+                {/* <SimpleModal label="depressed" color="secondary.dark" document="depression" /> */}
             </div>
         )
     }
--- client/src/components/simplemodal.jsx
+++ client/src/components/simplemodal.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import * as baseService from '../services/base';
 
-import pink from '@material-ui/core/colors/pink';
-import lightblue from '@material-ui/core/colors/lightblue';
-
-// const url = "https://bibles.org/v2/chapters/eng-CEV:1Cor.2/verses.js?start=5&end=7";
-// const url = "https://bibles.org/v2/verses/eng-GNTD:Acts.8.34.js";
-// const url = "https://bibles.org/v2/passages.js?q[]=john+3:1-5&version=eng-CEV"
 const proxyurl = "https://serene-crag-81882.herokuapp.com/"
 const BIBLE_API_KEY = process.env.BIBLE_API_KEY
-
-var parser = new DOMParser();
-
-var versecontent = [];
-var versetext = "";
 
 function rand() {
     return Math.round(Math.random() * 20) - 10;
@@ -46,13 +35,6 @@ const styles = theme => ({
     }
 });
 
-const colortheme = createMuiTheme({
-    palette: {
-        primary: pink,
-        secondary: lightblue,
-    }
-});
-
 class SimpleModal extends React.Component {
     state = {
         open: false,
@@ -60,7 +42,7 @@ class SimpleModal extends React.Component {
         abbr: "",
         start: "",
         end: "",
-        content: []
+        content: [],
     };
 
     // onClick of emotion button, 3 steps occur:
@@ -115,13 +97,12 @@ class SimpleModal extends React.Component {
         const { classes } = this.props;
         return (
             <div>
-                <MuiThemeProvider theme={colortheme}>
+                <MuiThemeProvider theme={this.props.theme}>
                     <Button style={getModalStyle()} variant="contained" color={this.props.color} onClick={this.handleOpen}>{this.props.label}</Button>
                 </MuiThemeProvider>
                 <Modal open={this.state.open} onClose={this.handleClose}>
                     <div style={getModalStyle()} className={classes.paper}>
                         <Typography variant="h6" id="modal-title">{this.state.reference}</Typography>
-                        {/* <Typography variant="subtitle1" id="simple-modal-description"><div dangerouslySetInnerHTML={{ __html: this.state.content }} /></Typography> */}
                         <Typography variant="subtitle1" id="simple-modal-description">{this.state.content}</Typography>
 
                     </div>
@@ -139,45 +120,3 @@ SimpleModal.propTypes = {
 const SimpleModalWrapped = withStyles(styles)(SimpleModal);
 
 export default SimpleModalWrapped;
-
-
-
-
-
-
-
-
-
-// let joined = versecontent.join(" ")
-// this.setState({
-//     content: joined
-// })
-
-
-
-
-
-
-
-
-
-// REGEX METHOD
-// .then(res => res.json())                                                // convert api json response to js
-//     .then(contents => {
-//         let versearray = contents.response.verses                           // extract array of verses objects
-
-//         versearray.forEach(verse => {                                       // loop through array
-//             let versetext = verse.text                                      // extract text value for each verse object (includes other unwanted items)
-//             console.log(versetext)
-
-//             let pattern = /<\/sup\b[^>]*>(.*?)<\/p>/
-//             let words = pattern.exec(versetext)[1]
-//             this.setState({
-//                 content: [...this.state.content, words]
-//             })
-//             console.log(this.state.content)
-//         })
-//     })
-//     .catch((e) => console.log(`Can’t access Bible API url. Error: ${e}`))
-
-{/* <Typography variant="subtitle1" id="simple-modal-description">{this.state.content}</Typography> */ }
