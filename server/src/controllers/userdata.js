b4d841f04a7e5bf89994f234aeec9fb3e3c1f1d4
Christy Welsh <christyannwelsh@gmail.com>
2019-06-20 16:37:07 -0700

    finalize userdata req

--- server/src/controllers/userdata.js
+++ server/src/controllers/userdata.js
@@ -1,8 +1,6 @@
 const getDB = require('../config/db').getDB;
 const UserdataController = {};
 
-let cursor;
-
 UserdataController.post = async (req, res) => {
     try {
         const db = getDB();
@@ -10,22 +8,21 @@ UserdataController.post = async (req, res) => {
         const tally = req.body.tally;
 
         // update if document exists, use upsert to create if doesn't yet exist
-            cursor = await db.collection('userdata').updateOne(
-                { reference: ref },
-                {
-                    $inc: { [tally]: 1 },
-                    $set: { reference: ref }
-                },
+        await db.collection('userdata').updateOne(
+            { reference: ref },
+            {
+                
+                $inc: { [tally]: 1 },
+                $set: { reference: ref },
+            },
 
-                { upsert: true }
-            )
-
-        console.log(cursor)
-
+            { upsert: true }
+        )
     } catch (e) {
         console.error(`Unable to issue request, ${e}`)
         return []
     }
+    res.send("User Data Added")
 }
 
 export default UserdataController;
