564ab712ffd104c04638c8fdae455aea5a9b5140
Christy Welsh <christyannwelsh@gmail.com>
2019-06-20 13:14:11 -0700

    add userdata collections to cloud db

--- server/src/controllers/userdata.js
+++ server/src/controllers/userdata.js
@@ -1,57 +1,30 @@
-import Userdata from '../db/models/userdata';
-
+const getDB = require('../config/db').getDB;
 const UserdataController = {};
+
+let cursor;
 
 UserdataController.post = async (req, res) => {
     try {
+        const db = getDB();
+        const ref = req.body.reference;
+        const tally = req.body.tally;
 
-        // check for existing reference before posting new
-        await Userdata.countDocuments({ reference: req.body.reference }, (err, count) => {
-            let ref = req.body.reference
-            let tally = req.body.tally
-            if (err) {
-                res.status(500).send(err)
-            } else {
+        // update if document exists, use upsert to create if doesn't yet exist
+            cursor = await db.collection('userdata').updateOne(
+                { reference: ref },
+                {
+                    $inc: { [tally]: 1 },
+                    $set: { reference: ref }
+                },
 
-                // if count > 0, reference already exists, therefore increment like/dislike on the reference
-                if (count > 0) {
-                    Userdata.updateOne({ reference: ref }, { $inc: { [tally]: 1 } }, (err, updated) => {
-                        if (err) {
-                            res.status(500).send(err)
-                        } else {
-                            console.log(updated)
-                            res.send("User Data Added")
-                        }
-                    })
+                { upsert: true }
+            )
 
-                    // if count is not > 0, reference doesn't exits, post a new reference with one tally (and include opposite field)
-                }
-                else {
-                    if (tally = 'likes') {
-                        Userdata.create({ reference: req.body.reference, likes: 1, dislikes: 0 }, (err, newdata) => {
-                            if (err) {
-                                res.status(500).send(err)
-                            } else {
-                                console.log(newdata)
-                                res.send("User data added")
-                            }
-                        })
-                    } else {
-                        Userdata.create({ reference: req.body.reference, likes: 0, dislikes: 1 }, (err, newdata) => {
-                            if (err) {
-                                res.status(500).send(err)
-                            } else {
-                                console.log(newdata)
-                                res.send("User data added")
-                            }
-                        })
-                    }
-                }
-            }
-        })
-    }
-    catch (err) {
-        console.log(err)
+        console.log(cursor)
+
+    } catch (e) {
+        console.error(`Unable to issue request, ${e}`)
+        return []
     }
 }
 
