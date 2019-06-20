5ff7abc0b3671730cacbe95252daea892704bd73
Christy Welsh <christyannwelsh@gmail.com>
2019-06-20 13:29:37 -0700

    separate files for local and cloud db reqs

--- server/src/controllers/userdatalocal.js
+++ server/src/controllers/userdatalocal.js
@@ -4,57 +4,56 @@ import Userdata from '../db/models/userdata';
 const UserdataController = {};
 
 
-// UserdataController.post = async (req, res) => {
-//     try {
+UserdataController.post = async (req, res) => {
+    try {
 
-//         // check for existing reference before posting new
-//         await Userdata.countDocuments({ reference: req.body.reference }, (err, count) => {
-//             let ref = req.body.reference
-//             let tally = req.body.tally
-//             if (err) {
-//                 res.status(500).send(err)
-//             } else {
+        // check for existing reference before posting new
+        await Userdata.countDocuments({ reference: req.body.reference }, (err, count) => {
+            let ref = req.body.reference
+            let tally = req.body.tally
+            if (err) {
+                res.status(500).send(err)
+            } else {
+                // if count > 0, reference already exists, therefore increment like/dislike on the reference
+                if (count > 0) {
+                    Userdata.updateOne({ reference: ref }, { $inc: { [tally]: 1 } }, (err, updated) => {
+                        if (err) {
+                            res.status(500).send(err)
+                        } else {
+                            console.log(updated)
+                            res.send("User Data Added")
+                        }
+                    })
 
-//                 // if count > 0, reference already exists, therefore increment like/dislike on the reference
-//                 if (count > 0) {
-//                     Userdata.updateOne({ reference: ref }, { $inc: { [tally]: 1 } }, (err, updated) => {
-//                         if (err) {
-//                             res.status(500).send(err)
-//                         } else {
-//                             console.log(updated)
-//                             res.send("User Data Added")
-//                         }
-//                     })
+                    // if count is not > 0, reference doesn't exits, post a new reference with one tally (and include opposite field)
+                }
+                else {
+                    if (tally = 'likes') {
+                        Userdata.create({ reference: req.body.reference, likes: 1, dislikes: 0 }, (err, newdata) => {
+                            if (err) {
+                                res.status(500).send(err)
+                            } else {
+                                console.log(newdata)
+                                res.send("User data added")
+                            }
+                        })
+                    } else {
+                        Userdata.create({ reference: req.body.reference, likes: 0, dislikes: 1 }, (err, newdata) => {
+                            if (err) {
+                                res.status(500).send(err)
+                            } else {
+                                console.log(newdata)
+                                res.send("User data added")
+                            }
+                        })
+                    }
+                }
+            }
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
 
-//                     // if count is not > 0, reference doesn't exits, post a new reference with one tally (and include opposite field)
-//                 }
-//                 else {
-//                     if (tally = 'likes') {
-//                         Userdata.create({ reference: req.body.reference, likes: 1, dislikes: 0 }, (err, newdata) => {
-//                             if (err) {
-//                                 res.status(500).send(err)
-//                             } else {
-//                                 console.log(newdata)
-//                                 res.send("User data added")
-//                             }
-//                         })
-//                     } else {
-//                         Userdata.create({ reference: req.body.reference, likes: 0, dislikes: 1 }, (err, newdata) => {
-//                             if (err) {
-//                                 res.status(500).send(err)
-//                             } else {
-//                                 console.log(newdata)
-//                                 res.send("User data added")
-//                             }
-//                         })
-//                     }
-//                 }
-//             }
-//         })
-//     }
-//     catch (err) {
-//         console.log(err)
-//     }
-// }
-
-// export default UserdataController;
+export default UserdataController;
