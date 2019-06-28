bc0349f72dd8245ed6c0d8202ad7b3b88baeea53
ChristyCakes <christyannwelsh@gmail.com>
2019-06-28 11:03:05 -0700

    add require dotenv

--- client/src/components/simplemodal.jsx
+++ client/src/components/simplemodal.jsx
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import * as baseService from '../services/base';
 import Thumbs from './thumbs'
 
+require('dotenv').config()
+
 const proxyurl = "https://serene-crag-81882.herokuapp.com/"
 const BIBLE_API_KEY = process.env.BIBLE_API_KEY
 
