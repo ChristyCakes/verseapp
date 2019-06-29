67c02dd34ee1a672ba2b74b1a4c22707a25a2677
ChristyCakes <christyannwelsh@gmail.com>
2019-06-29 15:31:47 -0700

    add webpack env plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,66 @@ const path = require('path');
 const CLIENT_DEST = path.join(__dirname, './client/dist');
 const Dotenv = require('dotenv-webpack');
 
+// module.exports = () => {
+//     return {
+//         plugins: [
+//             new Dotenv(),
+//             new webpack.DefinePlugin({
+//                 'process.env' : {
+//                     NODE_ENV: JSON.stringify(nodeenv)
+//                 }
+//             })
+//         ],
 module.exports = {
-    plugins: [
-        new Dotenv()
-    ],
-    devtool: 'source-map',
-    entry: ['@babel/polyfill', './client/src/index.js'],
-    output: { path: CLIENT_DEST, filename: 'bundle.js' },
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        publicPath: '/dist/images',
-                        outputPath: 'images',
-                    },
-                },
-            },
-            {
-                test: /\.(pdf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        publicPath: '/dist/docs',
-                        outputPath: 'docs',
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                loader: ['style-loader', 'css-loader'],
-            }
+        plugins: [
+            new Dotenv(),
+            new webpack.EnvironmentPlugin({
+                NODE_ENV: 'development',
+                DEBUG : true
+            })
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-};
+        devtool: 'source-map',
+        entry: ['@babel/polyfill', './client/src/index.js'],
+        output: { path: CLIENT_DEST, filename: 'bundle.js' },
+        module: {
+            rules: [
+                {
+                    test: /.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                        },
+                    },
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: '/dist/images',
+                            outputPath: 'images',
+                        },
+                    },
+                },
+                {
+                    test: /\.(pdf)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: '/dist/docs',
+                            outputPath: 'docs',
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    loader: ['style-loader', 'css-loader'],
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+    }
