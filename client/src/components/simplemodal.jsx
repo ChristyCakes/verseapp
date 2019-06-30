baed64590524e014d8c86b25f9d9dcd4c8f341d2
ChristyCakes <christyannwelsh@gmail.com>
2019-06-30 16:10:57 -0700

    add logs

--- client/src/components/simplemodal.jsx
+++ client/src/components/simplemodal.jsx
@@ -10,13 +10,6 @@ import Thumbs from './thumbs'
 const proxyurl = "https://serene-crag-81882.herokuapp.com/"
 const BIBLE_API_KEY = process.env.BIBLE_API_KEY
 
-
-//**************** */
-console.log(process.env.BIBLE_API_KEY)  //good
-
-
-
-
 function rand() {
     return Math.round(Math.random() * 20) - 10;
 }
@@ -78,35 +71,12 @@ class SimpleModal extends React.Component {
             // 2. call Bible api with reference, returns verse content
             .then(() => {
                 const url = `https://bibles.org/v2/chapters/eng-NASB:${this.state.abbr}/verses.js?start=${this.state.start}&end=${this.state.end}`
-
-
-
-                // **************
-                console.log(BIBLE_API_KEY)  //good
-
-
-
-
                 fetch(proxyurl + url, {
                     headers: new Headers({ 'Authorization': 'Basic ' + window.btoa(`${BIBLE_API_KEY}: x`) }),
                     redirect: "follow",
                 })
-                    .then(res => {
-
-
-
-                            // *******************                       
-                        console.log("fetch bible api res: ", res)
-                        console.log("fetch bible api res.json: ", res.json())
-                        
-
-
-
-                        res.json()
-                    })
+                    .then(res => res.json())
                     .then(contents => {
-
-
 
                         // **************
                         console.log("simplemodal fetch api contents: ", contents)  // undefined
@@ -120,7 +90,7 @@ class SimpleModal extends React.Component {
 
 
                         // ******************
-                        console.log("simplemodal fetch api contents: ", versearray)
+                        console.log("simplemodal fetch api versearray: ", versearray)
 
 
 
